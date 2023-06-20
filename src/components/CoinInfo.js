e7007aa4e36d7b4e0a24d79dc33cefbbfef96e32
Naveen Fogat <naveenf@google.com>
2023-06-20 13:52:57 +0530

    resolved line issue

--- src/components/CoinInfo.js
+++ src/components/CoinInfo.js
@@ -56,6 +56,7 @@ const CoinInfo = ({ coin }) => {
   const { currency } = CryptoState();
   const [flag, setFlag] = useState(false);
   const chartRef = useRef(null);
+  const chartInstanceRef = useRef(null); // Track the Chart instance
 
   const classes = useStyles();
 
@@ -80,17 +81,74 @@ const CoinInfo = ({ coin }) => {
     },
   });
 
+  useEffect(() => {
+    if (chartRef.current) {
+      if (chartInstanceRef.current) {
+        chartInstanceRef.current.destroy(); // Destroy the previous Chart instance
+      }
+
+      const chartInstance = new ChartJS(chartRef.current, {
+        type: "line",
+        data: {
+          labels: historicData.map((coin) => {
+            let date = new Date(coin[0]);
+            let time =
+              date.getHours() > 12
+                ? `${date.getHours() - 12}:${date.getMinutes()} PM`
+                : `${date.getHours()}:${date.getMinutes()} AM`;
+            return days === 1 ? time : date.toLocaleDateString();
+          }),
+          datasets: [
+            {
+              data: historicData.map((coin) => coin[1]),
+              label: `Price ( Past ${days} Days ) in ${currency}`,
+              borderColor: "#EEBC1D",
+            },
+          ],
+        },
+        options: {
+          elements: {
+            point: {
+              radius: 1,
+            },
+          },
+        },
+      });
+
+      chartInstanceRef.current = chartInstance; // Store the new Chart instance
+    }
+  }, [historicData, days, currency]);
+
   return (
     <ThemeProvider theme={darkTheme}>
       <div className={classes.container}>
-        {!historicData ? (
-          <CircularProgress
-            style={{ color: "gold" }}
-            size={250}
-            thickness={1}
-          />
+        {!historicData || flag === false ? (
+          <CircularProgress style={{ color: "gold" }} size={250} thickness={1} />
         ) : (
-          <><Line/></>
+          <>
+            <canvas ref={chartRef} id={`chart-${coin.id}`} />
+            <div
+              style={{
+                display: "flex",
+                marginTop: 20,
+                justifyContent: "space-around",
+                width: "100%",
+              }}
+            >
+              {chartDays.map((day) => (
+                <SelectButton
+                  key={day.value}
+                  onClick={() => {
+                    setDays(day.value);
+                    setFlag(false);
+                  }}
+                  selected={day.value === days}
+                >
+                  {day.label}
+                </SelectButton>
+              ))}
+            </div>
+          </>
         )}
       </div>
     </ThemeProvider>
